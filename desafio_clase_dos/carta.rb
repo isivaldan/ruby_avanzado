2641f3764b77f13eb50dba5e27ca21ddaac5ae1a
Isidora Valenzuela <isi.valenzuela@gmail.com>
2020-07-05 14:40:13 -0400

    cambio

--- desafio_clase_dos/carta.rb
+++ desafio_clase_dos/carta.rb
@@ -6,6 +6,12 @@ class Carta
     def initialize(numero, pinta)
       @numero = numero
       @pinta = pinta
+      if numero <1 ||  numero > 13
+        raise RangeError, "Ingrese un valor del 1 al 13 "
+    end
+    if pinta !="C" && pinta !="D" && pinta !="E" && pinta !="T"
+      raise RangeError, "Las pintas son C, D, E, T"
+  end
     end
   
     def to_s
@@ -14,6 +20,8 @@ class Carta
     end
   
 end
+#carta1 = Carta.new(14, "G")
+carta2 = Carta.new(13, "C")
 
 
 

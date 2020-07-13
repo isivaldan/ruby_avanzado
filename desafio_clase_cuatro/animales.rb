f3ac8e2888076b5337e8dc6024f5266521094aca
Isidora Valenzuela <isi.valenzuela@gmail.com>
2020-07-13 15:23:06 -0400

    correcciones

--- desafio_clase_cuatro/animales.rb
+++ desafio_clase_cuatro/animales.rb
@@ -33,3 +33,97 @@ module Alimentacion
     end
   end
 end
+
+#----
+# Crear la clase Animal con un atributo nombre. Agregar un método getter para el atributonombre(1 punto)
+# Crear las clases Ave, Mamifero e Insecto. Ambas heredan de Animal(1 punto)
+# Crear las clases Pingüino, Paloma y Pato. Las tres heredan de Ave(1 punto)
+# Crear las clases Perro, Gato y Vaca. Las tres heredan de Mamifero (1 punto)
+# Crear las clases Mosca, Mariposa y Abeja. Las tres heredan de Insecto(1 punto)
+# Incluye los módulos en cada subclase utilizando la instrucción include con el objetivo dedefinir las habilidades 
+# y tipo de alimentación de cada animal (2 puntos)
+
+class Animal
+  attr_reader :nombre
+  def initialize(nombre)
+    @nombre = nombre
+  end
+end
+
+class Ave < Animal
+  include Habilidades
+  include Alimentacion
+end
+
+class Mamifero < Animal
+  include Habilidades
+  include Alimentacion
+end
+
+class Insecto < Animal
+  include Habilidades
+  include Alimentacion
+end
+
+class Pinguino < Ave
+  include Habilidades::Nadador
+  include Habilidades::Caminante
+  include Alimentacion::Carnivoro
+end
+
+class Paloma < Ave
+  include Habilidades::Volador
+  include Habilidades::Caminante
+  include Alimentacion::Carnivoro
+end
+
+class Pato < Ave
+  include Habilidades::Nadador
+  include Habilidades::Caminante
+  include Habilidades::Volador
+  include Alimentacion::Herbivoro
+end
+
+class Perro < Mamifero
+  include Habilidades::Nadador
+  include Habilidades::Caminante
+  include Alimentacion::Carnivoro
+end
+
+class Gato < Mamifero
+  
+  include Habilidades::Caminante
+  include Alimentacion::Carnivoro
+end
+
+class Vaca < Mamifero
+  
+  include Habilidades::Caminante
+  include Alimentacion::Herbivoro
+end
+
+class Mosca < Insecto
+  include Habilidades::Volador
+  include Habilidades::Caminante
+  include Alimentacion::Carnivoro
+end
+
+class Mariposa < Insecto
+  include Habilidades::Volador
+  include Habilidades::Caminante
+  include Alimentacion::Herbivoro
+end
+
+class Abeja < Insecto
+  include Habilidades::Volador
+  include Habilidades::Caminante
+  include Alimentacion::Herbivoro
+end
+
+
+
+pato = Pato.new("PATO")
+
+puts pato.nadar
+puts pato.caminar
+puts pato.comer
--- desafio_clase_cuatro/person.rb
+++ desafio_clase_cuatro/person.rb
@@ -1,3 +1,4 @@
+
 class Person
   def initialize(first, last, age, type)
     @first_name = first
@@ -8,24 +9,65 @@ class Person
   def birthday
     @age += 1
   end
-  def talk
-    if @type == "Student"
-      puts "Aquí es la clase de programación con Ruby?"
-    elsif @type == "Teacher"
-      puts "Bienvenidos a la clase de programación con Ruby!"
-    elsif @type == "Parent"
-      puts "Aquí es la reunión de apoderados?"
-    end
-  end
   def introduce
-    if @type == "Student"
-      puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
-    elsif @type == "Teacher"
-      puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-    elsif @type == "Parent"
-      
-      puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #
-      {@last_name}."
-    end
+    "Mi nombre es #{@first_name} #{@last_name}."
   end
 end
+class Teacher<Person
+  def introduce
+    puts "Hola alumnos."
+    super
+  end
+  def talk
+    "Bienvenidos a la clase de programación con Ruby!"
+  end
+  
+end
+class Student<Person
+  def introduce
+    puts "Hola profesor."
+    super
+  end
+  def talk
+    "Aquí es la clase de programación con Ruby?"
+  end
+end
+class Parent<Person
+  def introduce
+    puts "Hola. Soy uno de los apoderados."
+    super
+  end
+  def talk
+    "Aquí es la reunión de apoderados?"
+  end
+end
+# def talk
+#   if @type == "Student"
+#     puts "Aquí es la clase de programación con Ruby?"
+#   elsif @type == "Teacher"
+#     puts "Bienvenidos a la clase de programación con Ruby!"
+#   elsif @type == "Parent"
+#     puts "Aquí es la reunión de apoderados?"
+#   end
+# end
+# def introduce
+#   if @type == "Student"
+#     puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+#   elsif @type == "Teacher"
+#     puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+#   elsif @type == "Parent"
+
+#     puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #
+#     {@last_name}."
+#   end
+# end
+student = Student.new("Isi","ValDan",19,"estudiante")
+parent = Parent.new("Alex","Oroz",25,"apoderado")
+teacher =Teacher.new("Seba","Val",35,"profesor")
+
+puts student.talk
+puts student.introduce
+puts parent.talk
+puts parent.introduce
+puts teacher.talk
+puts teacher.introduce
